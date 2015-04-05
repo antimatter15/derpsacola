3957b1926eb685c558f49944331f76e643f6e6f8
Kevin Kwok <antimatter15@gmail.com>
2015-04-05 01:22:30 -0400

    Initial Commit

---
+++ derpsacola/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  derpsacola
+//
+//  Created by Kevin Kwok on 4/5/15.
+//  Copyright (c) 2015 Kevin Kwok. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
