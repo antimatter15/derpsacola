960990ba6be724a4fdab1957ee13912e6f2fd118
Ben Vest <bvest@innervent.net>
2017-07-13 12:03:36 -0700

    derp

--- derpsacola/AppDelegate.swift
+++ derpsacola/AppDelegate.swift
@@ -10,83 +10,82 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-        for app in App.allApps() {
-            if app.isHidden() != false { continue }
-            println(app.title());
-            for win in app.allWindows()! {
-                print("---");
-                println(win.title())
-                for name in win.element.getAttributeNames()! {
-                    println("\t", name, win.element.getAttribute(name) as CFTypeRef?)
-                }
-            }
+  
+  @IBOutlet weak var window: NSWindow!
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    for app in App.allApps() {
+      if app.isHidden() != false { continue }
+      print(app.title() ?? "No Title Found");
+      for win in app.allWindows()! {
+        print("---");
+        print(win.title() ?? "Window has no title")
+        for name in win.element.getAttributeNames()! {
+          print("\t", name, win.element.getAttribute(name) as CFTypeRef? ?? "Couldn't get attribute for \(name)")
         }
+      }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-    
-    func oldUnusedCode(){
-        
-        let systemWideElement : AXUIElement = AXUIElementCreateSystemWide().takeRetainedValue()
-        
-        var elPtr = UnsafeMutablePointer<Unmanaged<AXUIElement>?>.alloc(1)
-        let error = AXUIElementCopyElementAtPosition(systemWideElement, 656.843750, 849.500000, elPtr)
-        
-        if(error == Int32(kAXErrorSuccess)){
-            print("woot")
-            
-            
-        }
-        println(elPtr)
-        
-        for name in systemWideElement.getAttributeNames()! {
-            println(name, systemWideElement.getAttribute(name) as CFTypeRef?);
-        }
-        
-        println("----------------");
-        
-        //        systemWideElement.getAttributes(<#property: String#>)
-        
-        let focusedApp : AXUIElementRef = systemWideElement.getAttribute("AXFocusedApplication")!;
-        //        let focusedApp = elPtr.memory!.takeRetainedValue();
-        
-        
-        for name in focusedApp.getAttributeNames()! {
-            println(name);
-            if(name == NSAccessibilityChildrenAttribute){
-                println("!!!!!!!!omg children");
-                
-            }
-            let t:CFTypeRef? = focusedApp.getAttribute(name);
-            //            switch AXValueGetType(t) {
-            //            case AXUIElementGetTypeID():
-            //                println("ui el")
-            //
-            //            }
-            if(t != nil){
-                if(CFGetTypeID(t) == AXUIElementGetTypeID()){
-                    println("----ui el");
-                }else if(CFGetTypeID(t) == CFArrayGetTypeID()){
-                    println("----array", t);
-                }else{
-                    println("----", t);
-                }
-            }else{
-                println("0000 nil");
-            }
-            
-        }
-
-    }
-
+  }
+  
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+  
+  
+  func oldUnusedCode(){
+    //
+    //        let systemWideElement : AXUIElement = AXUIElementCreateSystemWide().takeRetainedValue()
+    //
+    //        var elPtr = UnsafeMutablePointer<Unmanaged<AXUIElement>?>.allocate(capacity: 1)
+    //        let error = AXUIElementCopyElementAtPosition(systemWideElement, 656.843750, 849.500000, elPtr)
+    //
+    //        if error == AXError.success.rawValue {
+    //            print("woot")
+    //
+    //
+    //        }
+    //        print(elPtr)
+    //
+    //        for name in systemWideElement.getAttributeNames()! {
+    //            print(name, systemWideElement.getAttribute(name) as CFTypeRef?);
+    //        }
+    //
+    //        print("----------------");
+    //
+    //        //        systemWideElement.getAttributes()
+    //
+    //        let focusedApp : AXUIElement = systemWideElement.getAttribute("AXFocusedApplication")!;
+    //        //        let focusedApp = elPtr.memory!.takeRetainedValue();
+    //
+    //
+    //        for name in focusedApp.getAttributeNames()! {
+    //            print(name);
+    //            if(name == NSAccessibilityChildrenAttribute){
+    //                print("!!!!!!!!omg children");
+    //
+    //            }
+    //            let t:CFTypeRef? = focusedApp.getAttribute(name);
+    //            //            switch AXValueGetType(t) {
+    //            //            case AXUIElementGetTypeID():
+    //            //                print("ui el")
+    //            //
+    //            //            }
+    //            if(t != nil){
+    //                if(CFGetTypeID(t) == AXUIElementGetTypeID()){
+    //                    print("----ui el");
+    //                }else if(CFGetTypeID(t) == CFArrayGetTypeID()){
+    //                    print("----array", t);
+    //                }else{
+    //                    print("----", t);
+    //                }
+    //            }else{
+    //                print("0000 nil");
+    //            }
+    //
+    //        }
+    //
+  }
+  
 }
 
--- derpsacola/Extensions.swift
+++ derpsacola/Extensions.swift
@@ -1,69 +1,75 @@
 import Foundation
 
 public extension AXUIElement {
+  
+  public func getAttribute<T>(_ property: String) -> T? {
+    var ptr: CFTypeRef?
+    if AXUIElementCopyAttributeValue(self, property as CFString, &ptr) != AXError.success { return nil }
+    return ptr.map { $0 as! T }
+  }
+  
+  public func getAttributes<T: AnyObject>(_ property: String) -> [T]? {
+    var count: CFIndex = 0
+    if AXUIElementGetAttributeValueCount(self, property as CFString, &count) != AXError.success { return nil }
+    if count == 0 { return [T]() }
     
-    public func getAttribute<T>(property: String) -> T? {
-        var ptr: Unmanaged<AnyObject>?
-        if AXUIElementCopyAttributeValue(self, property, &ptr) != AXError(kAXErrorSuccess) { return nil }
-        return ptr.map { $0.takeRetainedValue() as T }
-    }
+    var ptr: CFArray?
+    if AXUIElementCopyAttributeValues(self, property as CFString, 0, count, &ptr) != AXError.success { return nil }
+    if ptr == nil { return nil }
     
-    public func getAttributes<T: AnyObject>(property: String) -> [T]? {
-        var count: CFIndex = 0
-        if AXUIElementGetAttributeValueCount(self, property, &count) != AXError(kAXErrorSuccess) { return nil }
-        if count == 0 { return [T]() }
-        
-        var ptr: Unmanaged<CFArray>?
-        if AXUIElementCopyAttributeValues(self, property, 0, count, &ptr) != AXError(kAXErrorSuccess) { return nil }
-        if ptr == nil { return nil }
-        
-        let array: Array<AnyObject>? = ptr?.takeRetainedValue()
-        if array == nil { return nil }
-        return array as? [T]
-    }
-    
-    public func getAttributeNames() -> [String]? {
-        var ptr: Unmanaged<CFArray>?;
-        AXUIElementCopyAttributeNames(self, &ptr)
-        return ptr?.takeRetainedValue() as? [String]
-    }
-    
-    public func setAttribute<T: AnyObject>(property: String, value: T) -> Bool {
-        return AXUIElementSetAttributeValue(self, property, value) != AXError(kAXErrorSuccess)
-    }
-    
-    public subscript(property: String) -> AnyObject? {
-        get { return getAttribute(property) as AnyObject? }
-        set { setAttribute(property, value: newValue!) }
-    }
-    
+    let array: Array<AnyObject>? = ptr as! Array<AnyObject>
+    if array == nil { return nil }
+    return array as? [T]
+  }
+  
+  public func getAttributeNames() -> [String]? {
+    var ptr: CFArray?
+    AXUIElementCopyAttributeNames(self, &ptr)
+    return ptr as? [String]
+  }
+  
+  public func setAttribute<T: AnyObject>(_ property: String, value: T) -> Bool {
+    return AXUIElementSetAttributeValue(self, property as CFString, value) != AXError.success
+  }
+  
+  public subscript(property: String) -> AnyObject? {
+    get { return getAttribute(property) as AnyObject? }
+    set { setAttribute(property, value: newValue!) }
+  }
+  
 }
 
 public extension AXValue {
-    
-    public class func fromPoint(var p: CGPoint) -> AXValue {
-        return AXValueCreate(kAXValueCGPointType, &p).takeRetainedValue()
+  
+  public class func fromPoint(_ p: CGPoint) -> AXValue {
+    var p = p
+    return AXValueCreate(AXValueType(rawValue: kAXValueCGPointType)!, &p).unsafelyUnwrapped
+  }
+  
+  public class func fromSize(_ p: CGSize) -> AXValue {
+    var p = p
+    return AXValueCreate(AXValueType(rawValue: kAXValueCGSizeType)!, &p).unsafelyUnwrapped
+  }
+  
+  public class func fromRect(_ p: CGRect) -> AXValue {
+    var p = p
+    return AXValueCreate(AXValueType(rawValue: kAXValueCGRectType)!, &p).unsafelyUnwrapped
+  }
+  
+  public class func fromRange(_ p: CFRange) -> AXValue {
+    var p = p
+    return AXValueCreate(AXValueType(rawValue: kAXValueCFRangeType)!, &p).unsafelyUnwrapped
+  }
+  
+  public func convertToStruct<T>() -> T? {
+    let ptr = UnsafeMutablePointer<T>.allocate(capacity: 1)
+    let success = AXValueGetValue(self, AXValueGetType(self), ptr)
+    let val = ptr.pointee
+    ptr.deinitialize()
+    guard success else {
+      return nil
     }
-    
-    public class func fromSize(var p: CGSize) -> AXValue {
-        return AXValueCreate(kAXValueCGSizeType, &p).takeRetainedValue()
-    }
-    
-    public class func fromRect(var p: CGRect) -> AXValue {
-        return AXValueCreate(kAXValueCGRectType, &p).takeRetainedValue()
-    }
-    
-    public class func fromRange(var p: CFRange) -> AXValue {
-        return AXValueCreate(kAXValueCFRangeType, &p).takeRetainedValue()
-    }
-    
-    public func convertToStruct<T>() -> T? {
-        let ptr = UnsafeMutablePointer<T>.alloc(1)
-        let success = AXValueGetValue(self, AXValueGetType(self), ptr)
-        let val = ptr.memory
-        ptr.destroy()
-        if success != 0 { return val }
-        return nil
-    }
-    
+    return val
+  }
+  
 }
